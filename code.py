2bf73785298f922f9309dbf6bb9776260cf5fd72
Elliot Salas <71795098+Bluesock420@users.noreply.github.com>
2020-09-28 03:36:36 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,24 @@
 def main():
+  
+  # Project-1-Team-Compression
+#This is team compression, this is what we did
+def main():
+
+#Open's the text file
+my_file = open "Code_File.txt"
+
+#Reading the txt file as a string
+my_string = Code_File.read()
+
+#The string is printed
+print(len(my_string))
+
+Creates a loop in the program
+for i in range(0,len(my_string)):
+  val = findcode(my_string[i])
+  print(i," " ,my)string[i]," ',val)
+  
 
 
-main():
+
+main()
